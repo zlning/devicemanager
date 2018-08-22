62e74added0fd048f67596735062bc5f0ff7673f
zlning <1730386018@qq.com>
2018-08-22 20:25:01 +0800

    添加DeviceServer初始化

--- com/device/DeviceServer.java
+++ com/device/DeviceServer.java
@@ -9,10 +9,16 @@ public class DeviceServer extends Thread{
 	private int ClientPort;
 	private String ClientBeatIp;
 	private int ClientBeatPort;
+	private int ListenBeatPort;
+	private int ListenReturnPort = 9092;
+	private int SshPort = 22;
 	private DatagramSocket BeatSocket;
-	public DeviceServer(String name){
+	
+	public DeviceServer(String name, String ip, int port){
 		ClientName = name;
-		BeatSocket = new DatagramSocket(9091);
+		ClientBeatIp = ip;
+		ClientBeatPort = port;
+		BeatSocket = new DatagramSocket(Port);
 	}
 	@Override
         public void run(){
@@ -22,15 +28,42 @@ public class DeviceServer extends Thread{
         		DatagramPacket datagramPacket = new DatagramPacket(buf, buf.length); // 1024
         		BeatSocket.receive(datagramPacket);
 			System.out.println("ip:"+datagramPacket.getAddress().getHostAddress()+" port:"+datagramPacket.getPort());
-			ClientBeatIp = datagramPacket.getAddress().getHostAddress();
-			ClientBeatPort = datagramPacket.getPort();
+			//ClientBeatIp = datagramPacket.getAddress().getHostAddress();
+			//ClientBeatPort = datagramPacket.getPort();
 		}
 		BeatSocket.close();
 	}
-	public void PostCommand(){
-
-	} 
 	public void GetClientIpPort(){
-	
+		//ListenReturnport
+		PostCommand("getclientipport", ListenReturnport, null);
+		byte[] buf = new byte[1024];
+		ReturnSocket = new DatagramSocket(ListenBeatPort);
+		DatagramPacket datagramPacket = new DatagramPacket(buf, buf.length); // 1024
+                ReturnSocket.receive(datagramPacket);
+                System.out.println("ip:"+datagramPacket.getAddress().getHostAddress()+" port:"+datagramPacket.getPort());
+                ClientIp = datagramPacket.getAddress().getHostAddress();
+                ClientPort = datagramPacket.getPort();
+		ReturnSocket.close();
+	}
+	public void Connect(DeviceServer dest){
+		PostCommand("connect", dest.ClientIp, dest.ClientPort);
+	}
+	private void PostCommand(String s,String param1,int param2){
+		if(s.equals("getclientipport")){
+			DatagramSocket datagramSocket = new DatagramSocket();
+        		String data = s+" "+param2;
+        		DatagramPacket packet = new DatagramPacket(data.getBytes(), data.getBytes().length,InetAddress.getByName(ClientBeatIp),ClientBeatPort);
+        		datagramSocket.send(packet);
+        		datagramSocket.close();
+			
+		}else if(s.equals("connect")){
+			DatagramSocket datagramSocket = new DatagramSocket();
+                        String data = s+" "+param1+" "+param2;                                                                                               
+                        DatagramPacket packet = new DatagramPacket(data.getBytes(), data.getBytes().length,InetAddress.getByName(ClientBeatIp),ClientBeatPort); 
+                        datagramSocket.send(packet);
+                        datagramSocket.close();
+		}else{
+			System.out.println("DeviceServerManager Post Do Not Supported Commond");
+		}
 	}
 }
--- com/device/DeviceServerManager.java
+++ com/device/DeviceServerManager.java
@@ -2,18 +2,97 @@ package com.device;
 
 public class DeviceServerManager{
         private static DeviceConsole DC = null;
-	private 
+	private int DeviceServerManagerDemoPort=9095;
+	private DatagramSocket DeviceServerManagerDemoSocket;
+	private List<DeviceServer> DeviceServerList;	
+
 	public static void main(String[] args){
 		//DC = new DeviceConsole();
 		DC.start();
+		DeviceServerManagerDemo.start();
+		while(true){
+			Iterator<DeviceServer> it = DeviceServerList.iterator();
+                	while (it.hasNext()) {
+                        	DeviceServer next = it.next();
+				if(next.getState() == Thread.State.NEW){
+					next.start();
+				}
+                	}
+		}
 	}
 	public DeviceServerManager(){
+		DeviceServerList = new ArrayList<DeviceServer>();
 		DC = new DeviceConsole();
+		DeviceServerManagerDemoSocket = new DatagramSocket(DeviceServerManagerDemoPort);
 	}
 	public void ShowDevices(){
-	
+		Iterator<DeviceServer> it = DeviceServerList.iterator();	
+		while (it.hasNext()) {
+			System.out.println(it.next());
+		}
 	}
-	public void Connectp2p(){
-	
+	public void HoleNat(String sour, String dest, int holeport){
+		Iterator<DeviceServer> it = DeviceServerList.iterator();
+                while (it.hasNext()) {
+			DeviceServer next = it.next();
+                        if(next.ClientName.equals(sour)){
+				Iterator<DeviceServer> it1 = DeviceServerList.iterator(); 
+				while (it.hasNext()) {
+                        		DeviceServer next1 = it.next();
+                        		if(next1.ClientName.equals(dest)){
+						next.Connect(next1, holeport);
+						break;
+					}
+				}
+				break;
+			}
+                }
+	}
+	public void AddNewDevice(String name, String Ip, int Port){
+		Iterator<DeviceServer> it = DeviceServerList.iterator();                                                                          
+                while (it.hasNext()) {                                                                                                            
+                        DeviceServer next = it.next();
+			if(next.ClientIp.equals(Ip)){
+				return;
+			}
+		}
+		DeviceServerList.add(new DeviceServer(name, Ip, Port));
+	}
+	private void ExeCommand(String s){
+		StringTokenizer st = new StringTokenizer(s);
+                String command = st.nextToken();
+		
+		if(st.hasMoreElements()){
+                	String param1=st.nextToken();
+		if(st.hasMoreElements())
+			String param2=st.nextToken();
+		if(command.equals("creatnewdeviceserver")){
+			AddNewDevice(command, param1, Integer.parseInt(param2));
+		}else{
+			System.out.println("DeviceServerManager demo Do Not Supported Commond");
+		}
+	}
+	private class DeviceServerManagerDemo extends Thread{
+		@Override
+        	public void run(){
+		        byte[] buf = new byte[1024];
+                	while(true){
+                        	DatagramPacket datagramPacket = new DatagramPacket(buf, buf.length); // 1024
+                        	BeatSocket.receive(datagramPacket);
+                        	System.out.println("ip:"+datagramPacket.getAddress().getHostAddress()+" port:"+datagramPacket.getPort());
+                        	//ClientBeatIp = datagramPacket.getAddress().getHostAddress();
+                        	//ClientBeatPort = datagramPacket.getPort();
+				ExeCommand(new String(buf,0,datagramPacket.getLength())+" "+datagramPacket.getAddress().getHostAddress()
+					   +" "+datagramPacket.getPort());
+                
+				DatagramSocket datagramSocket1 = new DatagramSocket(DeviceServerManagerDemoPort);
+                		String data = "accept";
+                		DatagramPacket packet = new DatagramPacket(data.getBytes(), data.getBytes().length, datagramPacket.getAddress().getHostAddress()
+									   ,datagramPacket.getPort());
+                		datagramSocket1.send(packet);
+                		datagramSocket1.close();
+                	}
+                	DeviceServerManagerDemoSocket.close(); 
+		}
 	}	
 }
